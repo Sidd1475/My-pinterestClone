0596b65766537656db887a82c234289f5a60fa93
Sidd1475 <167200325+Sidd1475@users.noreply.github.com>
2025-07-13 01:34:39 +0530

    Removed exposed secrets

--- routes/users.js
+++ routes/users.js
@@ -11,7 +11,10 @@ console.log('DEBUG URI =', process.env.MONGO_URI);
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
-});
+})
+.then(() => console.log("MongoDB Atlas Connected"))
+.catch((err) => console.error("MongoDB connection error:", err));
+
 const userSchema = new mongoose.Schema({
   username: {
     type: String,
